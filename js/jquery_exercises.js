40d13cad05a8feb4d52c72cdf5c62f7091207804
trevormullis12 <trevor.mullis12@gmail.com>
2020-05-12 13:50:51 -0500

    Jquery Selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,22 @@
 "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//
+// var title = $('#title').html();
+// alert(title);
+
+// $('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+
+var title = $('#title').html();
+var paragraph = $('#paragraph').html();
+var list = $('li').html();
+
+alert(title);
+
+var everything = $('#title, #paragraph, li');
+
+everything.css('background-color', 'yellow')
