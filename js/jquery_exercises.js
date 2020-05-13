6003d84f4917c1d3d813483becac12fe775cc39d
trevormullis12 <trevor.mullis12@gmail.com>
2020-05-13 10:48:35 -0500

    Jquery Events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -9,14 +9,28 @@
 
 // $('.codeup').css('border', '1px solid red');
 
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
+//
+// var title = $('#title').html();
+// var paragraph = $('#paragraph').html();
+// var list = $('li').html();
+//
+// alert(title);
+//
+// var everything = $('#title, #paragraph, li');
+//
+// everything.css('background-color', 'yellow')
 
-var title = $('#title').html();
-var paragraph = $('#paragraph').html();
-var list = $('li').html();
+$('h1').click(function() {
+    $(this).css('background-color', 'blue');
+})
 
-alert(title);
+$('p').dblclick(function() {
+    $(this).css('font-size', '18px');
+})
 
-var everything = $('#title, #paragraph, li');
-
-everything.css('background-color', 'yellow')
+$('li').hover(function () {
+    $(this).css('color','red');
+},function() {
+    $(this).css('color', 'black');
+})
